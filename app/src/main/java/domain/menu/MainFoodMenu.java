752c6362fc29c5c27825412bd6d8db726013cbf3
이윤빈 <kanado20000@naver.com>
2025-01-28 18:32:10 +0900

    feat: 메인메뉴 데이터를 저장하기 위해 json 파일 도입하고 MainFoodMenu 클래스에서 json 파일 reader하는 생성자 구현

--- app/src/main/java/domain/menu/MainFoodMenu.java
+++ app/src/main/java/domain/menu/MainFoodMenu.java
@@ -3,17 +3,45 @@ package domain.menu;
 
 
 import domain.food.MainFood;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 
 
 
 public class MainFoodMenu {
-    private final List<MainFood> mainFoods;
+    public final List<MainFood> mainFoods;
 
-    public MainFoodMenu(List<MainFood> mainFoods) {
+    public MainFoodMenu() throws IOException, ParseException {
+        List<MainFood> mainFoods = new ArrayList<>();
+        JSONParser parser = new JSONParser();
 
+        // JSON 파일을 읽기
+        Reader reader = new InputStreamReader(
+                getClass().getClassLoader().getResourceAsStream("main-menu.json")
+        );
+        JSONArray jsonArray = (JSONArray) parser.parse(reader);
+
+        // JSON 배열을 순회하면서 MainFood 객체 생성
+        for (Object obj : jsonArray) {
+            JSONObject jsonObject = (JSONObject) obj;
+            String name = (String) jsonObject.get("name");
+            int price = ((Long) jsonObject.get("price")).intValue();
+            int gram = ((Long) jsonObject.get("gram")).intValue();
+
+            MainFood mainFood = new MainFood(name, price, gram);
+            mainFoods.add(mainFood);
+        }
 
         this.mainFoods = mainFoods;
     }
+
 }
