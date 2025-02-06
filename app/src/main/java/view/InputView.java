879c6e4e886343ac362e8ed565fa8beab1492944
이윤빈 <kanado20000@naver.com>
2025-02-07 00:39:32 +0900

    feature: Scanner 객체가 여러개 만들어지는 것을 방지하기 위해 static final로 선언

--- app/src/main/java/view/InputView.java
+++ app/src/main/java/view/InputView.java
@@ -6,7 +6,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    Scanner scanner = new Scanner(System.in);
+    // Scanner 객체가 여러개 만들어지는 것을 방지하기 위해 static final로 선언
+    static final Scanner scanner = new Scanner(System.in);
 
     public String readStringAnswer() {
         return scanner.nextLine();
